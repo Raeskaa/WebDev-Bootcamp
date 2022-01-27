295f21aa73fc0ee88ba58a02546891a54ca74aff
Raeskaa <t.mahesh@iitg.ac.in>
2022-01-27 22:40:20 +0530

    Dice-Game

---
+++ Chapter 7/Drum-Kit/index.js
@@ -0,0 +1,6 @@
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+	document.querySelectorAll("button")[i].addEventListener("click", function () {
+	this.style.backgroundColor= "pink"
+		// what do when clicked
+	});
+}
