545bc8e08a3b5f63f5a261761b464b14b39e5526
Raeskaa <t.mahesh@iitg.ac.in>
2022-01-27 14:26:59 +0530

    Dice-Roll Completed

---
+++ Chapter 7/Readme.md
@@ -0,0 +1,3 @@
+###### Javascript Resource [link](https://docs.google.com/document/d/1JPhiKdQbu2IooJI0Me1ftSX5j2ttFXJ3hQH-3EAWwxQ/edit?usp=sharing)
+
+###### Task 1- Making a Dice Roll Game
--- Chapter 7/script.js
+++ Chapter 7/script.js
@@ -1,54 +1,21 @@
-var playerOne = 1;
-var playerTwo = 1;
+var playerone = Math.round(Math.random() * 6) + 1;
+var playertwo = Math.round(Math.random() * 6) + 1;
 
-function someFunction(){
-var x = Math.random();
-playerOne = Math.round(6*x) +1;
-playerTwo = Math.round(6*x) +1;
- }
- window.onload = someFunction();
- 
-if(playerOne > playerTwo) {
-    document.querySelector("h1").innerHTML = "Player One Wins!";
-}
-if(playerOne < playerTwo) {
-    document.querySelector("h1").innerHTML = "Player Two Wins!";
-}
-if(playerOne = 1){
-    document.getElementsByClassName("dice")[0].src = "images/dice1.png";
-}
-if(playerOne = 2){
-    document.getElementsByClassName("dice")[0].src = "images/dice2.png";
-}
-if(playerOne = 3){
-    document.getElementsByClassName("dice")[0].src = "images/dice3.png";
-}
-if(playerOne = 4){
-    document.getElementsByClassName("dice")[0].src = "images/dice4.png";
-}
-if(playerOne = 5){
-    document.getElementsByClassName("dice")[0].src = "images/dice5.png";
-}
-if(playerOne = 6){
-    document.getElementsByClassName("dice")[0].src = "images/dice6.png";
-}
-if(playerTwo = 1){
-    document.getElementsByClassName("dice")[0].src = "images/dice1.png";
-}
-if(playerTwo = 2){
-    document.getElementsByClassName("dice")[0].src = "images/dice2.png";
-}
-if(playerTwo = 3){
-    document.getElementsByClassName("dice")[0].src = "images/dice3.png";
-}
-if(playerTwo = 4){
-    document.getElementsByClassName("dice")[0].src = "images/dice4.png";
-}
-if(playerTwo = 5){
-    document.getElementsByClassName("dice")[0].src = "images/dice5.png";
-}
-if(playerTwo = 6){
-    document.getElementsByClassName("dice")[0].src = "images/dice6.png";
-}
+function butTon() {
+	var nameOne = prompt("Player One Name: ");
+	var nameTwo = prompt("Player Two Name: ");
 
+	document.querySelectorAll("p")[0].innerHTML = nameOne;
+	document.querySelectorAll("p")[1].innerHTML = nameTwo;
 
+	document.querySelector(".img1").src = "images/dice" + playerone + ".png";
+	document.querySelector(".img2").src = "images/dice" + playertwo + ".png";
+
+    if (playerone > playertwo) {
+		document.querySelector("h1").innerHTML = nameOne + " wins!";
+	} else if (playerone < playertwo) {
+		document.querySelector("h1").innerHTML = nameTwo + " wins!";
+	} else {
+		document.querySelector("h1").innerHTML = "Draw!";
+	}
+}
