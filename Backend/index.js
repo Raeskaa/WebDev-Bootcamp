c3c62c51298a8dca8866c7b7a5b4c32ab5f3de44
Raeskaa <t.mahesh@iitg.ac.in>
2022-02-06 14:21:25 +0530

    edits

--- Backend/index.js
+++
@@ -1,7 +0,0 @@
-var superheroes = require("superheroes");
-var supervillain = require("supervillain");
-
-var mySuperhero = superheroes.random();
-var mySupervillain = supervillain.random();
-console.log(mySuperhero);
-console.log(mySupervillain);
