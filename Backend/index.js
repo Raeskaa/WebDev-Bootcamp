ca8f978065f929e8cebe73ae1b4caa8fe72a414b
Raeskaa <t.mahesh@iitg.ac.in>
2022-02-06 13:28:57 +0530

    Intro to npm packages

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,7 @@
 var superheroes = require("superheroes");
+var supervillain = require("supervillain");
 
 var mySuperhero = superheroes.random();
+var mySupervillain = supervillain.random();
 console.log(mySuperhero);
+console.log(mySupervillain);
